fc0290caca9546cb94ce6aac9720999f07e0a8d6
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2018-07-09 18:46:00 +0530

    Reverse Number

---
+++ RevNum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class RevNum
+{
+public static void main(String [] args)
+{
+int num;
+Scanner sc = new Scanner(System.in);
+num = sc.nextInt();
+int revnum=0;
+while(num>0)
+{
+int rem;
+rem = num%10;
+revnum = revnum*10+rem;
+num = num/10;
+}
+System.out.println(revnum);
+}
+}
