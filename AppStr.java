38ac56ce406d5c4ee409840f0627ea4c97cb91c8
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2018-07-09 18:59:23 +0530

    Append Dot

---
+++ AppStr.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AppStr
+{
+public static void main(String [] args)
+{
+String input;
+Scanner sc = new Scanner(System.in);
+input = sc.next();
+input+=sc.nextLine();
+input = input.concat(".");
+System.out.println(input);
+}
+}
